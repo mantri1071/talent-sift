e02c4e268e655a6dd649be8d5252fa812e427ba5
Mantri Sumanth <mantrisumanth1@gmail.com>
2025-09-24 16:21:44 +0530

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -8,14 +8,12 @@ import JobFormStep1 from './components/JobFormStep1';
 import ResumeList from './components/ResumeList';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/resumes" element={<ResumeList />} />
-        <Route path="/jobform" element={<JobFormStep1 />} />
-        <Route path="/resumes" element={<Existing />} />
-      </Routes>
-    </BrowserRouter>
-
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/resumes" element={<ResumeList />} />
+      <Route path="/jobform" element={<JobFormStep1 />} />
+      <Route path="/existing" element={<Existing />} /> {/* ✅ Different path */}
+    </Routes>
+  </BrowserRouter>
 );
